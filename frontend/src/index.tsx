46a04a32239670224d431ef2fa8c5f328845944c
BokaiWang <bk.wang1989@gmail.com>
2024-05-10 21:26:31 -0400

    Implement react router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App";
 import "./assets/styles/custom.scss";
+import HomePage from "./pages/HomePage";
 import reportWebVitals from "./reportWebVitals";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomePage />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
