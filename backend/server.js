ae6e9426cfd7a2a0d2b830d2edecbffd83c55601
BokaiWang <bk.wang1989@gmail.com>
2024-11-08 20:13:59 -0500

    Add user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import connectDB from "./config/db.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 const port = process.env.PORT || 4000;
 
@@ -14,6 +15,8 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 
