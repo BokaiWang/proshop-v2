f9012fdc699d4123ba24a311f5ecbb034a19a5c1
BokaiWang <bk.wang1989@gmail.com>
2024-11-23 21:17:56 -0500

    Implement create and get orders api

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,24 +1,71 @@
 import asyncHandler from "../middleware/asyncHandler.js";
+import Order from "../models/orderModel.js";
 
 // @desc   Create new order
 // @route  POST /api/orders
 // @access Private
 const addOrderItems = asyncHandler(async (req, res) => {
-  return res.send("Add order items");
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error("No order items");
+  } else {
+    const order = new Order({
+      orderItems: orderItems.map(({ _id, name, qty, image, price }) => ({
+        name,
+        qty,
+        image,
+        price,
+        product: item._id,
+      })),
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingAddress,
+      totalPrice,
+    });
+  }
+
+  const createdOrder = await order.save();
+  res.status(201).json(createdOrder);
 });
 
 // @desc   Get logged in user orders
 // @route  GET /api/orders/my-orders
 // @access Private
 const getMyOrders = asyncHandler(async (req, res) => {
-  return res.send("Get logged in user orders");
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json(orders);
 });
 
 // @desc   Get order by ID
 // @route  GET /api/orders/:id
 // @access Private/Admin
 const getOrderById = asyncHandler(async (req, res) => {
-  return res.send("Get order by id");
+  // order does not have user name and email, so we need to populate it
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (order) {
+    res.status(200).json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc   Update order to paid
